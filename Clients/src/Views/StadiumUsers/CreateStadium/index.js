12c3a0123e7142f58afa282dce7ffbe9d8ac3a69
PKLBest89X <Phoummisay@gmail.com>
2021-06-06 12:50:17 +0700

    new component

---
+++ Clients/src/Views/StadiumUsers/CreateStadium/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageLayout from '../../../Components/PageLayout'
+
+const CreateStadium = () => {
+    return (
+        <PageLayout title="ສ້າງເດີ່ນຂອງທ່ານ">
+            
+        </PageLayout>
+    );
+};
+
+export default CreateStadium;
