991e23f011eaed1b12dda3506edddf4cbba97424
PKLBest89X <Phoummisay@gmail.com>
2022-02-24 21:42:30 +0700

    update

--- Clients/src/Layouts/SidebarLayout/Header.js
+++ Clients/src/Layouts/SidebarLayout/Header.js
@@ -1,70 +1,72 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { onSmDownClose } from '../../Slices/Features/ToggleDrawer/toggleSlice';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { onSmDownClose } from "../../Slices/Features/ToggleDrawer/toggleSlice";
+import { useDispatch } from "react-redux";
 import {
-  ListItemIcon,
-  ListItem,
-  IconButton,
-  Typography,
+    ListItemIcon,
+    ListItem,
+    IconButton,
+    Typography,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(() => ({
-  link: {
-    textDecoration: "none",
-    display: "flex",
-    width: "100%",
-    color: "black",
-    alignItems: "center",
-  },
-  icon: {
-    marginRight: '-8px'
-  },
-}))
+    link: {
+        textDecoration: "none",
+        display: "flex",
+        width: "100%",
+        color: "black",
+        alignItems: "center",
+    },
+    icon: {
+        marginRight: "-8px",
+    },
+}));
 
 const Header = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
+    const classes = useStyles();
+    const dispatch = useDispatch();
     return (
         <div className="header">
-        <ListItem>
-          <Link className={classes.link} to="#">
-            <ListItemIcon className={classes.icon}>
-              <IconButton
-                edge="start"
-                className={classes.menuButton}
-                color="inherit"
-                aria-label="open drawer"
-                onClick={(event) => {
-                  if (
-                    event.type === "keydown" &&
-                    (event.key === "Tab" || event.key === "Shift")
-                  ) {
-                    return;
-                  }
-                  dispatch(onSmDownClose())
-                }}
-                onKeyDown={(event) => {
-                  if (
-                    event.type === "keydown" &&
-                    (event.key === "Tab" || event.key === "Shift")
-                  ) {
-                    return;
-                  }
-                  dispatch(onSmDownClose())
-                }}
-              >
-                {<MenuIcon />}
-              </IconButton>
-            </ListItemIcon>
-            <Typography className={classes.title} variant="h4" noWrap>
-              PK-SPORT
-            </Typography>
-          </Link>
-        </ListItem>
-      </div>
+            <ListItem>
+                <Link className={classes.link} to="#">
+                    <ListItemIcon className={classes.icon}>
+                        <IconButton
+                            edge="start"
+                            className={classes.menuButton}
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={(event) => {
+                                if (
+                                    event.type === "keydown" &&
+                                    (event.key === "Tab" ||
+                                        event.key === "Shift")
+                                ) {
+                                    return;
+                                }
+                                dispatch(onSmDownClose());
+                            }}
+                            onKeyDown={(event) => {
+                                if (
+                                    event.type === "keydown" &&
+                                    (event.key === "Tab" ||
+                                        event.key === "Shift")
+                                ) {
+                                    return;
+                                }
+                                dispatch(onSmDownClose());
+                            }}
+                        >
+                            {<MenuIcon />}
+                        </IconButton>
+                    </ListItemIcon>
+                    <Typography className={classes.title} variant="h4" noWrap>
+                        PK-SPORT
+                    </Typography>
+                </Link>
+            </ListItem>
+        </div>
     );
 };
 
