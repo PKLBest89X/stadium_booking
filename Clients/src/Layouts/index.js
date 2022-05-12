1c6987419c0bef58fd8b46d6db33af5186f29cb0
PKLBest89X <Phoummisay@gmail.com>
2022-05-12 13:45:04 +0700

    update

--- Clients/src/Layouts/index.js
+++ Clients/src/Layouts/index.js
@@ -10,92 +10,92 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useShallowEqualSelector } from "../Components/useShallowEqualSelector";
 import { useDispatch } from "react-redux";
 import {
-  onSmUpOpen,
-  onSmUpClose,
-  onSmDownClose,
+    onSmUpOpen,
+    onSmUpClose,
+    onSmDownClose,
 } from "../Slices/Features/ToggleDrawer/toggleSlice";
 
 const marginMainContainer = 64;
 const useStyles = makeStyles((theme) => ({
-  pageStyles: {
-    minHeight: "100%",
-    // transition: '400ms ease-in-out',
-    [theme.breakpoints.up("lg")]: {
-      marginLeft: 250,
+    pageStyles: {
+        minHeight: "100%",
+        // transition: '400ms ease-in-out',
+        [theme.breakpoints.up("lg")]: {
+            marginLeft: 250,
+        },
+        [theme.breakpoints.down("md")]: {
+            marginLeft: marginMainContainer,
+        },
+        [theme.breakpoints.between("xs", "sm")]: {
+            marginLeft: 0,
+        },
     },
-    [theme.breakpoints.down("md")]: {
-      marginLeft: marginMainContainer,
-    },
-    [theme.breakpoints.between("xs", "sm")]: {
-      marginLeft: 0,
-    },
-  },
 
-  pageOpenTranslate: {
-    marginLeft: 250,
-    [theme.breakpoints.between("xs", "sm")]: {
-      marginLeft: 0,
+    pageOpenTranslate: {
+        marginLeft: 250,
+        [theme.breakpoints.between("xs", "sm")]: {
+            marginLeft: 0,
+        },
     },
-  },
-  pageCloseTranslate: {
-    marginLeft: marginMainContainer,
-    [theme.breakpoints.between("xs", "sm")]: {
-      marginLeft: 0,
+    pageCloseTranslate: {
+        marginLeft: marginMainContainer,
+        [theme.breakpoints.between("xs", "sm")]: {
+            marginLeft: 0,
+        },
     },
-  },
 }));
 
 const MainLayout = React.memo(() => {
-  const classes = useStyles();
-  const { data } = useShallowEqualSelector((state) => state.auth);
-  const { smUp } = useShallowEqualSelector((state) => state.toggle);
-  const dispatch = useDispatch();
+    const classes = useStyles();
+    const { data } = useShallowEqualSelector((state) => state.auth);
+    const { smUp } = useShallowEqualSelector((state) => state.toggle);
+    const dispatch = useDispatch();
 
-  const matchSm = useMediaQuery((theme) =>
-    theme.breakpoints.between("sm", "sm")
-  );
-  const matchMd = useMediaQuery((theme) =>
-    theme.breakpoints.between("md", "md")
-  );
-  const matchLg = useMediaQuery((theme) => theme.breakpoints.up("lg"));
+    const matchSm = useMediaQuery((theme) =>
+        theme.breakpoints.between("sm", "sm")
+    );
+    const matchMd = useMediaQuery((theme) =>
+        theme.breakpoints.between("md", "md")
+    );
+    const matchLg = useMediaQuery((theme) => theme.breakpoints.up("lg"));
 
-  useEffect(() => {
-    if (matchSm === true) {
-      dispatch(onSmUpClose());
-      dispatch(onSmDownClose());
-    }
-  }, [matchSm, dispatch]);
+    useEffect(() => {
+        if (matchSm === true) {
+            dispatch(onSmUpClose());
+            dispatch(onSmDownClose());
+        }
+    }, [matchSm, dispatch]);
 
-  useEffect(() => {
-    if (matchMd === true) {
-      dispatch(onSmUpClose());
-      dispatch(onSmDownClose());
-    }
-  }, [matchMd, dispatch]);
+    useEffect(() => {
+        if (matchMd === true) {
+            dispatch(onSmUpClose());
+            dispatch(onSmDownClose());
+        }
+    }, [matchMd, dispatch]);
 
-  useEffect(() => {
-    if (matchLg === true) {
-      dispatch(onSmUpOpen(true));
-    }
-  }, [matchLg, dispatch]);
+    useEffect(() => {
+        if (matchLg === true) {
+            dispatch(onSmUpOpen(true));
+        }
+    }, [matchLg, dispatch]);
 
-  const stategg = useRef(data);
-  useMemo(() => {
-    data.forEach((items) => (stategg.current = items.role));
-  }, [data]);
+    const stategg = useRef(data);
+    useMemo(() => {
+        data.forEach((items) => (stategg.current = items.role));
+    }, [data]);
 
-  return (
-    <>
-      <NavbarLayout />
-      <SidebarLayout />
-      <RoutesComponents
-        className={clsx(classes.pageStyles, {
-          [classes.pageOpenTranslate]: smUp,
-          [classes.pageCloseTranslate]: !smUp,
-        })}
-      />
-    </>
-  );
+    return (
+        <>
+            <NavbarLayout />
+            <SidebarLayout />
+            <RoutesComponents
+                className={clsx(classes.pageStyles, {
+                    [classes.pageOpenTranslate]: smUp,
+                    [classes.pageCloseTranslate]: !smUp,
+                })}
+            />
+        </>
+    );
 });
 
 export default MainLayout;
